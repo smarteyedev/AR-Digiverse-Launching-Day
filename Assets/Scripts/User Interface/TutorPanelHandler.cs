5d42b373b825db9e5d354da8250891ca4e5bd938
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2025-03-19 05:29:21 +0700

    update: tutor-animation

--- Assets/Scripts/User Interface/TutorPanelHandler.cs
+++ Assets/Scripts/User Interface/TutorPanelHandler.cs
@@ -1,21 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Smarteye.AR
 {
     public class TutorPanelHandler : PanelHandlerBase
     {
-        // Start is called before the first frame update
+        [Header("UI Animation")]
+        // Object pertama (UI)
+        public RectTransform objectOne;
+        public RectTransform targetPositionOne;
+        public Vector2 startPositionObjectOne;
+        public float durationOne = 1f;
+
+        [Space(5f)]
+        // Object kedua (UI)
+        public RectTransform objectTwo;
+        public RectTransform targetPositionTwo;
+        public Vector2 startPositionObjectTwo;
+        public float durationTwo = 1.5f;
+
         void Start()
         {
-
+            PlayAnimation(true);
         }
 
-        // Update is called once per frame
-        void Update()
+        public void PlayAnimation(bool isLoop)
         {
+            objectOne.anchoredPosition = startPositionObjectOne;
+            objectOne.localScale = Vector3.one * 0.5f;
 
+            Sequence sequence = DOTween.Sequence();
+
+            sequence.Append(objectOne.DOAnchorPos(targetPositionOne.anchoredPosition, durationOne).SetEase(Ease.OutBack));
+            sequence.Join(objectOne.DOScale(Vector3.one, durationOne).SetEase(Ease.OutBack));
+
+            sequence.AppendCallback(() =>
+            {
+                objectTwo.anchoredPosition = startPositionObjectTwo;
+
+                objectTwo.DOAnchorPos(targetPositionTwo.anchoredPosition, durationTwo).SetEase(Ease.OutQuad);
+            });
+
+            if (isLoop)
+            {
+                sequence.AppendInterval(2f);
+                sequence.AppendCallback(() =>
+                {
+                    objectOne.anchoredPosition = startPositionObjectOne;
+                    objectOne.localScale = Vector3.one * 0.5f;
+                    objectTwo.anchoredPosition = startPositionObjectTwo;
+                });
+
+                sequence.SetLoops(-1, LoopType.Restart);
+            }
         }
     }
 }
