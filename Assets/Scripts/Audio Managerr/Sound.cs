3a16f8ea08d550120872826621872c18cd06905b
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2025-03-19 09:22:01 +0700

    feat: audio-manager

---
+++ Assets/Scripts/Audio Managerr/Sound.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Smarteye.AR
+{
+    // Creator Instagram: @shantaufiq
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+    }
+}
